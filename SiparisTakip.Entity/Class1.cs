00d4947dad27350cd395e69e0545eace48a7ac15
Baki OZTURK <22.exe.94@gmail.com>
2021-03-28 19:43:45 +0300

    Adding All Project

---
+++ SiparisTakip.Entity/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SiparisTakip.Entity
+{
+    public class Class1
+    {
+    }
+}
---
+++ SiparisTakip.WPFLibrary/Class1.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace SiparisTakip.WPFLibrary
+{
+    public class Class1
+    {
+    }
+}
